86cab63b7b4bcbfe4cd3bc134388dbaf4811eabd
Paulo César dos Passos Pereira <paulo.passos.pereira1@outlook.com>
2026-07-02 14:19:03 -0300

    test(core): adiciona profile dev no teste de integracao para evitar falha no db

--- src/test/java/br/com/passos/api_blog_pessoal/ApiBlogPessoalApplicationTests.java
+++ src/test/java/br/com/passos/api_blog_pessoal/ApiBlogPessoalApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.passos.api_blog_pessoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ApiBlogPessoalApplicationTests {
 
 	@Test
